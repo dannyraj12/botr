bba56c2f2aa132c5f9932bb561fdc1be34579827
dannyraj12 <dannyraj12@outlook.com>
2026-05-30 07:47:03 +0530

    Create cleanup.py

---
+++ modules/cleanup.py
@@ -0,0 +1,12 @@
+import shutil
+import os
+
+
+def cleanup(path):
+    try:
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        elif os.path.isfile(path):
+            os.remove(path)
+    except:
+        pass
